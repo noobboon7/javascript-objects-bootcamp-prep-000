f299723a11c01abd657b2af803fa24bedf4ca5d3
Jerry Velazquez <jvr572@gmail.com>
2019-01-30 02:02:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
 var playlist = {nirvana: 'teen spirit' };
 
-function updatePlaylist()
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName = songTitle;
+  return playlist;
+}
+function 
