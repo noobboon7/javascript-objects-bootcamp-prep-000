22ee714e7ca54766479432f8014f062924567a55
Jerry Velazquez <jvr572@gmail.com>
2019-01-30 02:04:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,4 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
   return playlist;
 }
-function 
+function removeFromPlaylist(playlist, artistName){
+delete playlist.artistName;
+}
